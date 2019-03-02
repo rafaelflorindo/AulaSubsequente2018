6e2719a7cd119ad98c2d54f7dd4bd03e4d8b6a8b
rafaelflorindo <rafaelflorindo@hotmail.com>
2019-03-01 23:00:07 -0300

    Add files via upload
    Atualizaçῶao

--- Subsequente3info/Noticia.php
+++ Subsequente3info/Noticia.php
@@ -1,18 +1,57 @@
 <?php
 class Noticia{
 	public $autor, $descricao, $titulo;
-
+	
+	public function setAutor($autor) {
+		$this->autor = $autor;
+	}
+	public function setDescricao($descricao) {
+		$this->descricao = $descricao;
+	}
+	public function setTitulo($titulo) {
+		$this->titulo = $titulo;
+	}
+	
+	public function getAutor(){
+		return $this->autor;
+	}
+	public function getDescricao(){
+		return $this->descricao;
+	}
+	public function getTitulo(){
+		return $this->titulo;
+	}
+	
+	
 	public function cadastrar($dados){
 		list($autor, $descricao, $titulo) = $dados;
 
-		$this->autor = $autor;
-		$this->titulo = $titulo;
-		$this->descricao = $descricao;				
+		$this->setAutor($autor);
+		$this->setTitulo($titulo);
+		$this->setDescricao($descricao);				
 
-		echo "<hr>Título = ". $this->titulo;
-		echo "<br>Autor = ". $this->autor;
-		echo "<br>Descrição = ". $this->descricao;
+		echo "<hr>Título = ". $this->getTitulo();
+		echo "<br>Autor = ". $this->getAutor();
+		echo "<br>Descrição = ". $this->getDescricao();
 		echo "<hr>";
+		
+		
+		$sql = "insert into noticia (titulo, autor, descricao) values 
+		('{$this->getTitulo()}', '{$this->getAutor()}',
+		'{$this->getDescricao()}')";
+			
+		$conectar = new mysqli(
+		"localhost","root", "", "basenoticia");
+		
+		$inserir = $conectar->query($sql);
+
+		$numRegistro = $conectar->affected_rows;
+		
+		if($numRegistro == 1){
+			return 1;	
+		}else {
+			return 0;
+		}
 	}
 	public function alterar($dados){
 	}
@@ -23,4 +62,4 @@ class Noticia{
 	public function buscarTodos(){
 	}
 }
-?>
+?>	
--- Subsequente3info/controllerNoticia.php
+++ Subsequente3info/controllerNoticia.php
@@ -1,13 +1,24 @@
 <?php
-if(filter_input(INPUT_POST, "titulo") && filter_input(INPUT_POST, "autor") && filter_input(INPUT_POST, "descricao")) {
+if(filter_input(INPUT_POST, "titulo") 
+		&& filter_input(INPUT_POST, "autor") 
+		&& filter_input(INPUT_POST, "descricao")
+	) {
 	$titulo = filter_input(INPUT_POST, "titulo");
 	$autor = filter_input(INPUT_POST, "autor");
 	$descricao = filter_input(INPUT_POST, "descricao");
 	
 	$dados = array($autor, $descricao, $titulo);
+	
 	include_once("Noticia.php");
 	$notice = new Noticia();
-	$notice->cadastrar($dados);
+
+	$retorno = $notice->cadastrar($dados);
+	
+	if ($retorno == 1){
+		echo "Registro gravado com sucesso!!!";
+	}else{
+		echo "Erro ao gravar o registro!!!";	
+	}
 }else{
 	echo "Campos não foram preenchido";
 }	
