6579170914a2558165dab95e314bb91225ed40d2
rafaelflorindo <rafaelflorindo@hotmail.com>
2019-03-25 20:16:19 -0300

    Add files via upload

--- Noticia.php
+++ Noticia.php
@@ -1,6 +1,7 @@
 <?php
 
-/*Próxima aula:
+/*
+Próxima aula:
 Método construtor com os dados de conexão
 Método alterar
 Método listarTudo
@@ -10,7 +11,7 @@ Método ativar
 */
 
 class Noticia{
-	public $autor, $descricao, $titulo;
+	public $id, $autor, $descricao, $titulo;
 	
 	public function setAutor($autor) {
 		$this->autor = $autor;
@@ -21,7 +22,12 @@ class Noticia{
 	public function setTitulo($titulo) {
 		$this->titulo = $titulo;
 	}
-	
+	public function setId($id) {
+		$this->id = $id;
+	}
+	public function getId(){
+		return $this->id;
+	}
 	public function getAutor(){
 		return $this->autor;
 	}
@@ -65,11 +71,37 @@ class Noticia{
 	}
 	public function alterar($dados){
 	}
-	public function excluir($id){
+	public function inativar($dados){
+		list($id) = $dados;
+		$this->setId($id);
+		
+		$sql = "update noticia set ativo=0 where id =" . $this->getId();
+		
+		$listar = new mysqli("localhost", "root", "", "basenoticia");
+		$registro = $listar->query($sql);
+		$numRegistro = $listar->affected_rows;
+		
+		if($numRegistro == 1){
+			return 1;	
+		}else {
+				return 0;
+		}
 	}
 	public function buscarUm($id){
 	}
-	public function buscarTodos(){
+	public function buscarTodos($situacao){
+		
+		$sql = "select * from noticia where ativo = " . $situacao . " order by titulo asc";
+		
+		$listar = new mysqli("localhost", "root", "", "basenoticia");
+		$registro = $listar->query($sql);
+		
+		$dados = array();
+		while($linha = $registro->fetch_array()){
+			$dados[] = $linha;
+		}	
+		//print_r($dados);
+		return $dados;	
 	}
 }
 ?>	
